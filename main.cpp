6d4351a7539da082c964effd8aa5c76f01e249fc
martas <m.sroczynska@student.uw.edu.pl>
2017-11-28 14:21:19 +0100

    Added CXX flags

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "TestedClass.h"
 
 int main(){
+    TestedClass *obj = new TestedClass();
+    std::cout << "Created new TestedClass object, i = " << obj->getI() << std::endl;
+    delete obj;
 
 std::cout << "nic";
 }
